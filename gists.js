4cd3aa59e2db328b0decd769f999a67150853751
Bradley Sherman <sirrus233@gmail.com>
2015-04-27 20:38:35 -0700

    Querying gists from github works. Can receive a single page of results

--- gists.js
+++ gists.js
@@ -1,3 +1,17 @@
-function test() {
-    console.log("Hello again!");
+function queryGists() {
+    var numRequests = 1;
+    for (var i = 0; i < numRequests; i++) {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState === 4 && request.status === 200) {
+                httpRequestCallback(request);
+            }
+        }
+        request.open("GET", "https://api.github.com/gists/public", true);
+        request.send();
+    }
+}
+
+function httpRequestCallback(request) {
+    console.log(request.responseText);
 }
