4ffc978e75db829ca6d4726df81875ad4034f940
Bradley Sherman <sirrus233@gmail.com>
2015-04-27 20:54:22 -0700

    May now use a selector to query multiple pages of results, and print their descriptions to the console

--- gists.js
+++ gists.js
@@ -1,17 +1,34 @@
+// Returns how many pages of results the user wants to see, by reading the selector
+// in the html
+function getNumRequests() {
+    var selector = document.getElementById("pages");
+    return selector.options[selector.selectedIndex].value;
+}
+
+// Called when the search button is clicked. Formulates an arbitrary number of queries
+// of the Github API
 function queryGists() {
-    var numRequests = 1;
-    for (var i = 0; i < numRequests; i++) {
+    for (var i = 1; i <= getNumRequests(); i++) {
         var request = new XMLHttpRequest();
         request.onreadystatechange = function() {
             if (request.readyState === 4 && request.status === 200) {
                 httpRequestCallback(request);
             }
         }
-        request.open("GET", "https://api.github.com/gists/public", true);
+        request.open("GET", "https://api.github.com/gists/public?page="+i, true);
         request.send();
     }
 }
 
+// Called by the HttpRequest when it is finished communicating with the server
 function httpRequestCallback(request) {
-    console.log(request.responseText);
+    var gistArray = JSON.parse(request.responseText);
+    displayResults(gistArray);
+}
+
+// Displays gists to the screen. Input should be an array of gist objects
+function displayResults(gists) {
+    for (var i = 0; i < gists.length; i++) {
+        console.log(gists[i].description);
+    }
 }
