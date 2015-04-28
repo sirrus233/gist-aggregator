94e686c0b71a3226bcc89af2205a7056112edc0b
Bradley Sherman <sirrus233@gmail.com>
2015-04-28 02:39:10 -0700

    Added basic support for favoriting results

--- gists.js
+++ gists.js
@@ -43,15 +43,15 @@ function displayResults(gists) {
     for (var i = 0; i < gists.length; i++) {
         var url = gists[i].html_url;
         var desc = gists[i].description;
-        if (desc === "null" || desc === "") {
+        if (desc === null || desc === "") {
             desc = "Untitled";
         }
-        resultsDiv.appendChild(createResultElement(url, desc));
+        resultsDiv.appendChild(createResultElement(url, desc, true));
     }
 }
 
 // Builds a single result element to insert into the document
-function createResultElement(url, desc) {
+function createResultElement(url, desc, favorite) {
     // Simple div to hold result
     var result = document.createElement('div');
 
@@ -61,11 +61,45 @@ function createResultElement(url, desc) {
     link.appendChild(document.createTextNode(desc));
     result.appendChild(link); 
 
-    // Favorite button
+    // Button (to either favorite or unfavorite the link, based on input parameter)
     var fav = document.createElement('button');
     fav.type = "button";
-    fav.appendChild(document.createTextNode("Favorite"));
+    if (favorite) {
+        fav.onclick = function() {addFavorite(url, desc)};
+        fav.appendChild(document.createTextNode("Favorite"));
+    } else {
+        fav.onclick = function() {removeFavorite(url, desc)};
+        fav.appendChild(document.createTextNode("Unfavorite"));
+    }
     result.appendChild(fav); 
 
     return result;
+}
+
+// Populate the favorites list with anything inside localStorage
+function loadFavorites() {
+    // Clear old favorites
+    document.getElementById("favorites").innerHTML = "";
+
+    for (var i = 0; i < localStorage.length; i++) {
+        // Key is a url, mapped to a description
+        var key = localStorage.key(i);
+        var val = localStorage.getItem(key);
+        document.getElementById("favorites").appendChild(createResultElement(key, val, false));
+    }
+}
+
+// Clears the localStorage, then refreshes the favorites list, which should now be empty
+function deleteFavorites() {
+    localStorage.clear();
+    loadFavorites();
+}
+
+function addFavorite(url, desc) {
+    localStorage.setItem(url, desc);
+    loadFavorites();
+}
+
+function removeFavorite(url, desc) {
+    //TODO
 }
